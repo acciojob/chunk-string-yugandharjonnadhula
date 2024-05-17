2dec10b6b124d31235edfd65ec26c06d6d9b2bd5
yugandharjonnadhula <163026370+yugandharjonnadhula@users.noreply.github.com>
2024-05-17 18:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function stringChop(str, size) {
   // your code here
-	if (!str) 
-		return [];
+	if (!str) return [];
 	
 	const chunk=[];
 	if(let i=0;i<str.length;i=i+size){
-		chunk.push(str.slice(i,i+size));
+		chunk.push(str.slice(i,i+size))
 	}
 	return chunk;
 }
